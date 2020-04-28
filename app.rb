791e5c92d88023351e1dc835eab58247bf999b56
Craig Christopher Messi <craig.messi@gmail.com>
2020-04-28 02:53:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,10 +8,6 @@ class App < Sinatra::Base
 
   post '/' do
     @analyzed_text = TextAnalyzer.new(params[:user_text])
-    @word_count = @analyzed_text.count_of_words
-    @vowel_count = @analyzed_text.count_of_vowels
-    @constant_count = @analyzed_text.count_of_constants
-    @
     
     erb :results
   end
